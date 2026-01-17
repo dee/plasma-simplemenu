7102ea165f02134fdb36cc91408df695ebf1bc5f
Dee <33315+dee@users.noreply.github.com>
2026-01-17 09:13:39 +0100

    Added some model-related code

--- package/plugin/simplemenu.cpp
+++ package/plugin/simplemenu.cpp
@@ -3,7 +3,8 @@
 
 SimpleMenu::SimpleMenu(QObject* parent)
     : QAbstractListModel(parent),
-    m_filteredApps()
+    m_filteredApps(),
+    m_apps()
 {
 
 }
@@ -20,13 +21,28 @@ QVariant SimpleMenu::data(const QModelIndex &index, int role) const
         return QVariant();
     }
 
-    const QVariant &app = m_filteredApps.at(index.row());
+    const AppItem &app = m_filteredApps.at(index.row());
 
-    return app;
+    switch (role) {
+    case NameRole:
+        return app.name;
+    case IconRole:
+        return app.icon;
+    case ExecRole:
+        return app.exec;
+    case DescriptionRole:
+        return app.description;
+    default:
+        return QVariant();
+    }
 }
 
 QHash<int, QByteArray> SimpleMenu::roleNames() const
 {
-    QHash<int, QByteArray> roles{};
+    QHash<int, QByteArray> roles;
+    roles[NameRole] = "name";
+    roles[IconRole] = "icon";
+    roles[ExecRole] = "exec";
+    roles[DescriptionRole] = "description";
     return roles;
 }
--- package/plugin/simplemenu.h
+++ package/plugin/simplemenu.h
@@ -4,12 +4,26 @@
 #include <QObject>
 #include <QModelIndex>
 #include <QByteArray>
+#include <QString>
 
+struct AppItem {
+    QString name;
+    QString exec;
+    QString icon;
+    QString description;
+};
 
 class SimpleMenu : public QAbstractListModel
 {
     Q_OBJECT
 public:
+    enum Roles {
+        NameRole = Qt::UserRole + 1,
+        IconRole,
+        ExecRole,
+        DescriptionRole
+    };
+
     explicit SimpleMenu(QObject* parent = nullptr);
 
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
@@ -17,5 +31,5 @@ public:
     QHash<int, QByteArray> roleNames() const override;
 
 private:
-    QList<QString> m_filteredApps;
+    QList<AppItem> m_filteredApps, m_apps;
 };
